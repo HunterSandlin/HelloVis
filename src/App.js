c753ba475e188f02856d54b6e3c543b95f296f8b
Hunter Sandlin <huntergsandlin@gmail.com>
2021-06-17 18:15:30 -0400

    Added labels and made more reusable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 import BarChart from './BarChart';
 
 function App() {
+
+  const [data, setData] = useState([12, 5, 6, 6, 9, 10]);
+  const [width, setWidth] = useState(700);
+  const [height, setHeight] = useState(500);
+
+
   return (
     <>
-      <BarChart />
+      <div className="App">
+        <BarChart data={data} width={width} height={height} />
+      </div>
     </>
   );
 }
--- src/BarChart.js
+++ src/BarChart.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react';
 import * as d3 from "d3";
 
+
+/*
+
+BarChart made as a first introduction to d3 following:
+
+https://blog.logrocket.com/data-visualization-in-react-using-react-d3-c35835af16d0/
+
+
+*/
 class BarChart extends Component {
 
     componentDidMount() {
@@ -8,16 +17,14 @@ class BarChart extends Component {
     }
 
     drawChart() {
-        const data = [12, 5, 6, 6, 9, 10];
+        const data = this.props.data;
         let h = 400;
         let w = 700;
 
-        const svg = d3.select("body")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .style("margin-left", 100);
-                        
+        const svg = d3.select("body").append("svg")
+            .attr("width", this.props.width)
+            .attr("height", this.props.height);
+             
         svg.selectAll("rect")
             .data(data)
             .enter()
@@ -27,7 +34,17 @@ class BarChart extends Component {
             .attr("width", 65)
             .attr("height", (d, i) => d * 10)
             .attr("fill", "green");
-    }
+        
+        svg.selectAll("text")
+            .data(data)
+            .enter()
+            .append("text")
+            .text((d) => d)
+            .attr("x", (d, i) => i * 70)
+            .attr("y", (d, i) => h - (10 * d) - 3);
+    
+    
+        }
             
     render(){
         return <div id={"#" + this.props.id}></div>
